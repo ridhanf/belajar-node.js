bc8f95f63b9af6ad97182747545da4f7d44a09d2
Ridhan Fadhilah <ridhanfadhilah@gmail.com>
2020-11-19 00:12:25 +0700

    Requests & Responses w/o html

---
+++ nd04-http-requests/server.js
@@ -0,0 +1,17 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  console.log(req.url, req.method);
+
+  // set header content type
+  res.setHeader('Content-Type', 'text/html');
+
+  res.write('<head><link rel="stylesheet" href="#"></head>')
+  res.write('<p>hello, ninjas</p>');
+  res.write('<p>hello again, ninjas</p>');
+  res.end();
+});
+
+server.listen(3000, 'localhost', () => {
+  console.log('listening for request on port 3000');
+});
